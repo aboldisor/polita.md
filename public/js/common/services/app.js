fde4a2564b8cc3c47b9e4bfdd9cc530a2581954a
aboldisor@gmail.com <aboldisor@gmail.com>
2014-07-25 05:32:46 +0300

    translation module

--- public/js/common/services/app.js
+++ public/js/common/services/app.js
@@ -1,19 +1,79 @@
 
 
 (function(){
-    var app = angular.module('politaApp', ['ui.bootstrap']);
+    var app = angular.module('politaApp', ['ui.bootstrap','pascalprecht.translate'])
+
+     .config(['$translateProvider', function ($translateProvider) {
+            var ro = {
+                INDEX : {
+                    DRUGS    :"Medicamente",
+                    ANALYSIS : "Analize",
+                    HEALTHTRACKER : "Health-tracker",
+                    CHECKINSURANCE: "VERIFICA POLITA DE ASIGURARE",
+                    CHECKDOCTORREGISTRATION : "VERIFICA INREGISTRAREA LA MEDICUL DE FAMILIE"
 
 
-    app.controller('langController',
-        ['$scope',function($scope) {
-            $scope.currentLanguage ="ro";
-            $scope.changeLanguage = function (langKey) {
-                   $scope.currentLanguage = langKey;
+                }
             };
 
-        }]);
+            var ru = {
+                INDEX : {
+                    DRUGS    : "Справочник лекарств",
+                    ANALYSIS : "Медицинские анализы",
+                    HEALTHTRACKER : "Календарь здоровья",
+                    CHECKINSURANCE: "ПРОВЕРИТЬ СТРАХОВОЙ ПОЛИС",
+                    CHECKDOCTORREGISTRATION : "ПРОВЕРКА ЗАПИСИ ПАЦИЕНТОВ НА ПРИЕМ К ВРАЧУ"
+
+                }
+            };
+
+
+            $translateProvider.translations('ru', ru);
+            $translateProvider.translations('ro', ro);
+
+         $translateProvider.determinePreferredLanguage(function(){
+                var language = window.navigator.userLanguage || window.navigator.language;
+
+                switch(language) {
+                    case "ru":
+                    case "ru-ru":
+                    case "ru-RU":
+                    case "ru_RU":
+                        return "ru";
+                    case "ro":
+                    case "ro-ro":
+                    case "ro-RO":
+                    case "ro_RO":
+                        return "ro";
+                    default:
+                        return "ro";
+                }
+
+            });
+
+
+
+      }]);
+
+
+    //TODO set in client Controler Directory
+     app.controller('langController',  ['$scope','$translate',function($scope,$translate) {
+
+            $scope.currentLanguage = $translate.use();
+            $scope.changeLanguage = function (langKey) {
+                $translate.use(langKey);
+                $scope.currentLanguage = langKey;
+            };
+
+    }]);
+
+
+
 
 })();
+
+
+
 
 
 //-------------------------------------------------------------------
